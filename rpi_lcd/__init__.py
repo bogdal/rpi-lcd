f781f7de952c72306c7c329e964554d7debdc88c
Adam Bogdał <adam@bogdal.pl>
2016-09-08 21:22:47 +0200

    Add ability to break lines

--- rpi_lcd/__init__.py
+++ rpi_lcd/__init__.py
@@ -16,10 +16,11 @@ LINES = {
 
 class LCD(object):
 
-    def __init__(self, address=0x27, bus=1, width=20):
+    def __init__(self, address=0x27, bus=1, width=20, height=4):
         self.address = address
         self.bus = SMBus(bus)
         self.delay = 0.0005
+        self.height = height
         self.width = width
 
         self.write(0x33)
@@ -43,9 +44,20 @@ class LCD(object):
 
     def text(self, text, line, align='left'):
         self.write(LINES.get(line, LINES[1]))
+        text, other_lines = self.get_text_line(text)
         text = getattr(text, ALIGN_FUNC.get(align, 'ljust'))(self.width)
         for char in text:
             self.write(ord(char), mode=1)
+        if other_lines and line <= self.height - 1:
+            self.text(other_lines, line + 1, align=align)
+
+    def get_text_line(self, text):
+        line_break = self.width
+        if len(text) > self.width:
+            line_break = text[:self.width + 1].rfind(' ')
+        if line_break < 0:
+            line_break = self.width
+        return text[:line_break], text[line_break:].strip()
 
     def clear(self):
         self.write(CLEAR_DISPLAY)
