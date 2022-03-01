df029d149bfce2e8d7efdb7246e9dcd1cc0a59a2
Adam Bogdał <adam@bogdal.pl>
2022-03-01 12:21:41 +0100

    Bump version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='rpi-lcd',
-    version='0.0.2',
+    version='0.0.3',
     description='I²C LCD library for RaspberryPi',
     author='Adam Bogdał',
     author_email='adam@bogdal.pl',
